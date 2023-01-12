e17345ec376e7ff1ae2116a4c8f06de331270237
Wergugy <wergugy@gmail.com>
2023-01-11 20:23:40 -0800

    Start working on 'tab' feature.
    this will need to be done before the modules
    and other pages are built so im getting it done
    first. So far i have made the containers and
    appended them to the page. Will add some base
    css before moving forwards.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-
 const mainContainer = document.createElement('div');
+mainContainer.classList.add('mainContainer');
+const tabBox = document.createElement('div');
+tabBox.classList.add('tabBox');
+const bottomBar = document.createElement('div');
+bottomBar.classList.add('bottomBar');
+const homeTab = document.createElement('div');
+homeTab.classList.add('homeTab');
+const menuTab = document.createElement('div');
+menuTab.classList.add('menuTab');
+const contactTab = document.createElement('div');
+contactTab.classList.add('contactTab');
 
+tabBox.append(homeTab, menuTab, contactTab, bottomBar);
+mainContainer.append(tabBox);
+
+document.body.append(mainContainer);
