6769ec702309df804f302ebf06d7e9a3a96e4fa1
Wergugy <wergugy@gmail.com>
2023-01-11 18:47:42 -0800

    Add Prettier config and reformat files
    Changed the quote rule to single and reformatted
    the webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  devtool: "inline-source-map",
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Anything Almond",
+      title: 'Anything Almond',
     }),
   ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   optimization: {
-        runtimeChunk: 'single',
-      },
+    runtimeChunk: 'single',
+  },
 };
