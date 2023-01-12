6888014d55b38d89651cc076816a92e1fa3d37f4
Wergugy <wergugy@gmail.com>
2023-01-11 21:10:10 -0800

    Create a css file and import to js
    Also added some base styling - colors and outlines
    to make it easier to work with. Then put the tabs
    in their general spots. Now to get the events set.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 const mainContainer = document.createElement('div');
 mainContainer.classList.add('mainContainer');
 const tabBox = document.createElement('div');
@@ -5,11 +7,11 @@ tabBox.classList.add('tabBox');
 const bottomBar = document.createElement('div');
 bottomBar.classList.add('bottomBar');
 const homeTab = document.createElement('div');
-homeTab.classList.add('homeTab');
+homeTab.classList.add('home','tab');
 const menuTab = document.createElement('div');
-menuTab.classList.add('menuTab');
+menuTab.classList.add('menu','tab');
 const contactTab = document.createElement('div');
-contactTab.classList.add('contactTab');
+contactTab.classList.add('contact','tab');
 
 tabBox.append(homeTab, menuTab, contactTab, bottomBar);
 mainContainer.append(tabBox);
