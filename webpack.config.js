963a7edf416dac3b9cb76cbf2d575500802323db
Wergugy <wergugy@gmail.com>
2023-01-13 18:41:48 -0800

    Create module files
    3 module js files were made. Entry points on webpack
    were modified accordingly.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
   mode: 'development',
+  entry: {
+    index: './src/index.js',
+    home: './src/home.js',
+    menu: './src/menu.js',
+    contact: './src/contact.js'
+  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
@@ -14,7 +19,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
