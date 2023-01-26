af5b642f53e55ead2b2923e47e861f957598ca92
Wergugy <wergugy@gmail.com>
2023-01-25 22:25:42 -0800

    Have 'home' load initially
    home tab contents now load on page load. still
    need to sort the duplicate content bug. so far
    stops at only one additional.

--- src/home.js
+++ src/home.js
@@ -1,13 +1,18 @@
-const createHome = (page) => {
-  const heroBox = document.createElement('div');
-  heroBox.classList.add('heroBox');
-  const hero = document.createElement('div');
-  hero.classList.add('hero');
-  const heroMsg = document.createElement('div');
-  heroMsg.classList.add('heroMsg');
+const generate = (page) => {
+    const heroBox = document.createElement('div');
+    heroBox.classList.add('heroBox');
+    const hero = document.createElement('div');
+    hero.classList.add('hero');
+    const heroMsg = document.createElement('div');
+    heroMsg.classList.add('heroMsg');
 
-  heroBox.append(hero, heroMsg);
-  page.append(heroBox);
+    heroBox.append(hero, heroMsg);
+    page.append(heroBox);
 };
 
+const createHome = (current, page) => {
+    if (current !== 'home') {
+        generate(page);
+    }
+};
 export default createHome;
--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ mainContainer.classList.add('mainContainer');
 const tabBox = document.createElement('div');
 tabBox.classList.add('tabBox');
 const bottomBar = document.createElement('div');
-bottomBar.classList.add('bottomBar', 'baseColor');
+bottomBar.classList.add('bottomBar', 'home');
 const homeTab = document.createElement('div');
-homeTab.classList.add('home', 'tab');
+homeTab.classList.add('home', 'tab', 'current');
 const menuTab = document.createElement('div');
 menuTab.classList.add('menu', 'tab');
 const contactTab = document.createElement('div');
 contactTab.classList.add('contact', 'tab');
 const contentBox = document.createElement('div');
-contentBox.classList.add('contentBox', 'baseColor');
+contentBox.classList.add('contentBox', 'home');
 const page = document.createElement('div');
 page.classList.add('page');
 
@@ -25,63 +25,63 @@ document.body.append(mainContainer);
 
 const allTabs = Array.from(document.querySelectorAll('.tab'));
 
-const clearCurrent = () => {
-  if (allTabs.some((e) => e.classList.contains('current'))) {
-    allTabs
-      .find((e) => e.classList.contains('current'))
-      .classList.toggle('current');
-  }
-};
+let prevTab;
+createHome(null, page);
 
-const matchTheDrapes = (l) => {
-  const currentTab = Array.from(l.target.classList).filter(
-    (c) => c !== 'tab' && c !== 'current'
-  );
-  if (bottomBar.classList.contains('baseColor')) {
-    bottomBar.classList.replace('baseColor', currentTab);
-    contentBox.classList.replace('baseColor', currentTab);
-  } else
-    switch (currentTab.toString()) {
-      case 'home':
-        bottomBar.classList.replace(
-          bottomBar.classList.contains('menu') ? 'menu' : 'contact',
-          currentTab
-        );
-        contentBox.classList.replace(
-          contentBox.classList.contains('menu') ? 'menu' : 'contact',
-          currentTab
-        );
-        createHome(page);
-        break;
-      case 'menu':
-        bottomBar.classList.replace(
-          bottomBar.classList.contains('home') ? 'home' : 'contact',
-          currentTab
-        );
-        contentBox.classList.replace(
-          contentBox.classList.contains('home') ? 'home' : 'contact',
-          currentTab
-        );
-        break;
-      case 'contact':
-        bottomBar.classList.replace(
-          bottomBar.classList.contains('home') ? 'home' : 'menu',
-          currentTab
-        );
-        contentBox.classList.replace(
-          contentBox.classList.contains('home') ? 'home' : 'menu',
-          currentTab
-        );
-        break;
-      // no default
+const clearCurrent = () => {
+    if (allTabs.some((e) => e.classList.contains('current'))) {
+        allTabs
+            .find((e) => e.classList.contains('current'))
+            .classList.toggle('current');
     }
 };
 
+const matchTheDrapes = (l) => {
+    const currentTab = Array.from(l.target.classList).filter(
+        (c) => c !== 'tab' && c !== 'current'
+    ).toString();
+    switch (currentTab) {
+        case 'home':
+            createHome(prevTab, page);
+            bottomBar.classList.replace(
+                bottomBar.classList.contains('menu') ? 'menu' : 'contact',
+                currentTab
+            );
+            contentBox.classList.replace(
+                contentBox.classList.contains('menu') ? 'menu' : 'contact',
+                currentTab
+            );
+            break;
+        case 'menu':
+            bottomBar.classList.replace(
+                bottomBar.classList.contains('home') ? 'home' : 'contact',
+                currentTab
+            );
+            contentBox.classList.replace(
+                contentBox.classList.contains('home') ? 'home' : 'contact',
+                currentTab
+            );
+            break;
+        case 'contact':
+            bottomBar.classList.replace(
+                bottomBar.classList.contains('home') ? 'home' : 'menu',
+                currentTab
+            );
+            contentBox.classList.replace(
+                contentBox.classList.contains('home') ? 'home' : 'menu',
+                currentTab
+            );
+            break;
+        // no default
+    }
+    prevTab = currentTab
+};
+
 const selectCurrent = (l) => {
-  l.preventDefault();
-  clearCurrent();
-  l.target.classList.add('current');
-  matchTheDrapes(l);
+    l.preventDefault();
+    clearCurrent();
+    l.target.classList.add('current');
+    matchTheDrapes(l);
 };
 
 allTabs.forEach((e) => e.addEventListener('click', selectCurrent));
