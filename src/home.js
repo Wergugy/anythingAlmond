496c72990d1c7eb992b3dd775fe61164badde0cb
Wergugy <wergugy@gmail.com>
2023-01-31 04:49:47 -0800

    Link text to homepage.
    loads correctly. Some basic wrap rules added to
    not break whats already in place.

--- src/home.js
+++ src/home.js
@@ -1,11 +1,22 @@
+import homeHeroPic from './images/home-page-hero.jpg';
+import introMsg from './text/home-intro.txt';
+
 const generate = (page) => {
     const heroBox = document.createElement('div');
     heroBox.classList.add('heroBox');
+
     const hero = document.createElement('div');
     hero.classList.add('hero');
+
+    const heroImg = document.createElement('img');
+    heroImg.classList.add('heroImg');
+    heroImg.src = homeHeroPic;
+
     const heroMsg = document.createElement('div');
     heroMsg.classList.add('heroMsg');
+    heroMsg.textContent = introMsg;
 
+    hero.append(heroImg);
     heroBox.append(hero, heroMsg);
     page.append(heroBox);
 };
